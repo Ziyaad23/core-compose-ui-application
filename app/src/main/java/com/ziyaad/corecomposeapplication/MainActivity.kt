5825502ca3f23d1d592254ec4ed6c09782722f2b
Ziyaad <rhyman.ziyaad@gmail.com>
2021-09-04 17:13:53 +0400

    [+][ADD] Align composables with wrapped content

--- app/src/main/java/com/ziyaad/corecomposeapplication/MainActivity.kt
+++ app/src/main/java/com/ziyaad/corecomposeapplication/MainActivity.kt
@@ -3,10 +3,15 @@ package com.ziyaad.corecomposeapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.ziyaad.corecomposeapplication.ui.theme.CoreComposeApplicationTheme
 
@@ -14,25 +19,29 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CoreComposeApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen(){
+    Surface(
+        color = Color.DarkGray,
+        modifier = Modifier.fillMaxSize()) {
+            Surface(
+                color = Color.Magenta,
+                modifier = Modifier.wrapContentSize(align = Alignment.BottomStart)) {
+                Text(
+                    text = "Wrapped content",
+                    style = MaterialTheme.typography.h4
+                )
+            }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    CoreComposeApplicationTheme {
-        Greeting("Android")
-    }
+    MainScreen()
 }
